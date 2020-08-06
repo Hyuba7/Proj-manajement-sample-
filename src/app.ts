03f2ca987f0712469c9ccebe3aefd909578575c2
Hiroto Yuba <abyu629@gmail.com>
2020-08-07 02:58:49 +0900

    DOMのqueryselectorを変更

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,42 @@
 class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title' // inputのidを指定
+    ) as HTMLInputElement; 
+    
+    
+    this. descriptionInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+
+    this.mandayInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
 }
+const prjInput = new ProjectInput();
