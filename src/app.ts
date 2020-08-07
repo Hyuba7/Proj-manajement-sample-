b5789a718953dd729d33a864ab25192a41f5a8d8
Hiroto Yuba <abyu629@gmail.com>
2020-08-08 02:35:24 +0900

    DOMでTitlleのにゅ力をconsoleに出力

--- src/app.ts
+++ src/app.ts
@@ -8,35 +8,45 @@ class ProjectInput {
 
   constructor() {
     this.templateElement = document.getElementById(
-      'project-input'
+      'project-input',
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(
       this.templateElement.content,
-      true
+      true,
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
 
     this.titleInputElement = this.element.querySelector(
-      '#title' // inputのidを指定
-    ) as HTMLInputElement; 
-    
-    
-    this. descriptionInputElement = this.element.querySelector(
-      '#title'
+      '#title',
+    ) as HTMLInputElement;
+
+    this.descriptionInputElement = this.element.querySelector(
+      '#description',
     ) as HTMLInputElement;
 
     this.mandayInputElement = this.element.querySelector(
-      '#title'
+      '#manday',
     ) as HTMLInputElement;
 
+    this.configure();
     this.attach();
+  } //costructorでは要素への参照を行う
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
+
 const prjInput = new ProjectInput();
